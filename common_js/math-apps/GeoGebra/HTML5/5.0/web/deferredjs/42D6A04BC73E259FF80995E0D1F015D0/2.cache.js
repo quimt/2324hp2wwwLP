4b2b1db867e6b60097d8755c472c1ec6ec2455c3
quimt <126020181+quimt@users.noreply.github.com>
2023-11-26 07:02:48 -0500

    .

---
+++ common_js/math-apps/GeoGebra/HTML5/5.0/web/deferredjs/42D6A04BC73E259FF80995E0D1F015D0/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.web.runAsyncCallback2('Jhc(3450,1,wym);_.zd=function(){};oNl(Tc)(2);\n//# sourceURL=web-2.js\n')
---
+++ common_js/math-apps/GeoGebra/HTML5/5.0/webSimple/deferredjs/E53F795EA8A0304A7E8B319A4688851E/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.webSimple.runAsyncCallback2('zqb(2081,1,_Fh);_.ld=function(){};l5g(Tc)(2);\n//# sourceURL=webSimple-2.js\n')
